aee4366a0df8a60ed8fbce27d4c8abf12e3f7bf5
Nimi <Nimrobo@Gmail.com>
2022-12-20 18:20:10 +0200

    Initial commit

---
+++ app/src/main/java/com/example/studentlist/models/Student.java
@@ -0,0 +1,18 @@
+package com.example.studentlist.models;
+
+public class Student {
+    public String name;
+    public String id;
+    public String phone;
+    public String address;
+    public Boolean checkButton;
+
+    public Student(String name, String id, String phone,String address,Boolean checkButton) {
+        this.name = name;
+        this.id = id;
+        this.phone=phone;
+        this.address=address;
+        this.checkButton = checkButton;
+    }
+
+}
