5d00176473cca3308dba5db46a2ca34167a13a8e
Nimi <Nimrobo@Gmail.com>
2022-12-21 16:46:57 +0200

    Progress

--- app/src/main/java/com/example/studentlist/StudentRecyclerList.java
+++ app/src/main/java/com/example/studentlist/StudentRecyclerList.java
@@ -7,22 +7,26 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.studentlist.R;
+import com.example.studentlist.models.Demo;
 import com.example.studentlist.models.Student;
 
 import java.util.List;
 
 public class StudentRecyclerList extends AppCompatActivity {
-    List<Student> data;
+    List<Student> studentList;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recycler);
 
-        data = Model.
-        RecyclerView list = findViewById(R.id.studentrecycler_list);
+        studentList = Demo.instance().getStudents();
+        RecyclerView list = findViewById(R.id.recyclerStudentList);
         list.setHasFixedSize(true);
-
         list.setLayoutManager(new LinearLayoutManager(this));
         StudentRecyclerAdapter adapter = new StudentRecyclerAdapter();
         list.setAdapter(adapter);
+    }
+
+
 }
